73eee195aaae44d2ec5b0951750062dc89d28b1b
myallpic <myallpic@outlook.com>
2018-04-27 15:13:28 +0800

    Update pytime.py

--- img/pytime.py
+++ img/pytime.py
@@ -32,7 +32,7 @@ def get_net_data():
 def monfirst(filename):
     nowtime = time.strftime('%d %H:%M',time.localtime(time.time()))
     sec = time.localtime().tm_sec
-    if nowtime == '27 00:00':
+    if nowtime == '27 15:16':
         if sec < 10:
             f = open(filename, 'w')
             f.write('0')
